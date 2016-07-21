830ae1715a72d551aa58b9a31375516b71014961
David <david.alan.oconnor@gmail.com>
2016-07-21 18:53:17 +0100

    Removed old, unused code

--- fplot/fplot.py
+++ fplot/fplot.py
@@ -2,11 +2,10 @@ from functools import wraps
 from typing import Callable, Tuple
 
 from matplotlib import cm, pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D  # Axes3d's required for 3d plots,
-# even though it's not specifically required.
+# Axes3d's required for 3d plots, even though it's not specifically required.
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-
-#from numba import jit
+#import sympy
 
 
 # Todo broken atm.
@@ -22,92 +21,17 @@ def show2(f):
     return inner
 
 
-
-# @jit
-# def ols(x, y):
-#     """Simple OLS for two data sets."""
-#     M = x.size
-#
-#     x_sum = 0.
-#     y_sum = 0.
-#     x_sq_sum = 0.
-#     x_y_sum = 0.
-#
-#     for i in range(M):
-#         x_sum += x[i]
-#         y_sum += y[i]
-#         x_sq_sum += x[i] ** 2
-#         x_y_sum += x[i] * y[i]
-#
-#     slope = (M * x_y_sum - x_sum * y_sum) / (M * x_sq_sum - x_sum**2)
-#     intercept = (y_sum - slope * x_sum) / M
-#
-#     return slope, intercept
-#
-#
-# def test_linear(f):
-#         # Test if linear
-#         test_x = np.random.random(3)
-#         test_y = f(test_x)
-#         slope, intercept = ols(test_x, test_y)
-#
-#         if all(test_x * slope + intercept == test_y):
-#             return True
-#         return False
-
-def test_linear(f):
-    # Test if the function is linear; f(x) should vary proportionally with
-    # x.
-    x = np.arange(1, 5)  # todo this will fail for undefined vals.
-    print(f(x))
-    # Test if the diffs are all the same; they should be as long as x is
-    # equally-spaced.
-    diffs = np.diff(f(x))
-
-    if all(diffs[1:] == diffs[:-1]):
-        return True
-    return False
-
-
-def find_cat(f):
-    """Find a function's category"""
-    if test_linear(f):
-        return 'linear'
-
-
-def estimate_bounds(f):
-    """Estimate reasonable x and y display bounds for a function"""
-
-    if find_cat(f) == 'linear':
-        y_int = f(0)
-        return -5, 5
-
-    else:
-        x_test = np.logspace(-10, 10, 40)
-        y_test = f(x_test)
-        diffs = np.diff(y_test)
-        # diffs2 = np.diff(diffs)
-        # for x, diff in zip(x_test, diffs):
-            # print(x, diff)
-        # print(diffs2)
-
-        # todo use slope instead of just y diff (ie take x diff into account)
-        std = np.std(diffs)
-
-        return 0, 10
-
-
-def _set_grid(ax):
+def _set_grid_axes(ax):
     ax.grid(True)
 
-    ax.axhline(y=0, linewidth=1.5, color='k')
-    ax.axvline(x=0, linewidth=1.5, color='k')
+    ax.axhline(y=0, linewidth=1.5, color='darkslategrey')
+    ax.axvline(x=0, linewidth=1.5, color='darkslategrey')
     return ax
 
 
 def _set_misc(fig, ax, title: str, grid: bool, equal_aspect: bool) -> None:
     if grid:
-        _set_grid(ax)
+        _set_grid_axes(ax)
 
     if title:
         fig.suptitle(title, fontsize=14, fontweight='bold')
@@ -187,25 +111,27 @@ def _parametric3d(x: np.ndarray, y: np.ndarray, z: np.ndarray, color: str):
     return fig, ax
 
 
-def _two_in_one_out_helper(f: Callable[[float, float], float], x_min: float,
-                           x_max: float, resolution: int) -> \
+def _two_in_helper(f: Callable[[float, float], float], x_min: float,
+                   x_max: float, y_min: float, y_max: float,
+                   resolution: int) -> \
         Tuple[np.ndarray, np.ndarray, np.ndarray]:
     """Set up a mes grid for contour and evaluate the function for surface plots."""
     x = np.linspace(x_min, x_max, resolution)
-    y = np.linspace(x_min, x_max, resolution)
-
+    y = np.linspace(y_min, y_max, resolution)
     x, y = np.meshgrid(x, y)
-    z = f(x, y)
 
-    return x, y, z
+    return x, y, f(x, y)
 
 
 def contour(f: Callable[[float, float], float], x_min: float, x_max: float,
+            y_min: float=None, y_max: float=None, resolution=1e3,
             title: str=None, grid=True, show=True, equal_aspect=False) -> None:
     """Two inputs, one output."""
-    resolution = 1e3
-
-    x, y, z = _two_in_one_out_helper(f, x_min, x_max, resolution)
+    if not y_min:
+        y_min = x_min
+    if not y_max:
+        y_max = x_max
+    x, y, z = _two_in_helper(f, x_min, x_max, y_min, y_max, resolution)
 
     fig, ax = plt.subplots()
     ax.contour(x, y, z)
@@ -215,12 +141,15 @@ def contour(f: Callable[[float, float], float], x_min: float, x_max: float,
 
 
 def surface(f: Callable[[float, float], float], x_min: float, x_max: float,
-            title: str=None, show=True,
-            equal_aspect=False) -> None:
+            y_min: float=None, y_max: float=None, title: str=None, show=True,
+            equal_aspect=False, resolution=1e2) -> None:
     """Two inputs, one output."""
-    resolution = 1e2
+    if not y_min:
+        y_min = x_min
+    if not y_max:
+        y_max = x_max
 
-    x, y, z = _two_in_one_out_helper(f, x_min, x_max, resolution)
+    x, y, z = _two_in_helper(f, x_min, x_max, y_min, y_max, resolution)
 
     fig = plt.figure()
     ax = fig.gca(projection='3d')
@@ -231,20 +160,20 @@ def surface(f: Callable[[float, float], float], x_min: float, x_max: float,
 
 
 def vector(f: Callable[[float, float], Tuple[float, float]], x_min: float,
-           x_max: float, grid=True, title: str=None, show=True,
-           equal_aspect=False, stream=False, resolution: int=15) -> None:
+           x_max: float, y_min: float=None, y_max: float=None, grid=True,
+           title: str=None, show=True, equal_aspect=False, stream=False,
+           resolution: int=15) -> None:
     """Two inputs, two outputs. 2D vector plot. steam=True sets a streamplot with curved arrows
      instead of a traditionl vector plot."""
-    x = np.linspace(x_min, x_max, resolution)
-    y = np.linspace(x_min, x_max, resolution)
+    if not y_min:
+        y_min = x_min
+    if not y_max:
+        y_max = x_max
 
-    x, y = np.meshgrid(x, y)
-    i, j = f(x, y)
+    x, y, (i, j) = _two_in_helper(f, x_min, x_max, y_min, y_max, resolution)
     vec_len = (i**2 + j**2)**.5  # For color coding
 
     fig, ax = plt.subplots()
-
-    # plot_args = (x, y, i, j)
 
     if stream:
         ax.streamplot(x, y, i, j, color=vec_len, cmap=cm.inferno)
@@ -253,4 +182,8 @@ def vector(f: Callable[[float, float], Tuple[float, float]], x_min: float,
 
     _set_misc(fig, ax, title, grid, equal_aspect)
     _show_or_return(ax, show)
+
+
+def auto(f, x_min, x_max) -> None:
+    pass
 
